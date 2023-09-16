43321bf6b035fa862b5ea2a63387e8ce399563a6
wajihnajjar <wajih.najjar@esprit.tn>
2023-09-16 02:10:26 +0100

    Creating Dragable Compoenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Draggable from 'react-draggable';
 
 const createRandomNumber =()=>{
   return Math.floor(Math.random()*10)
 }
+
 function App() {
-  const [randomN , setRand] = useState([createRandomNumber(),createRandomNumber(),createRandomNumber(),createRandomNumber()])
+  const ref= useRef(null)
+  const [randomN , setRand] = useState([createRandomNumber(),createRandomNumber(),createRandomNumber(),createRandomNumber(),'+','-','*','/'])
+  const [test,useTest] = useState([1])
+  useEffect(()=>{
+    console.log(ref.current.children)
+    for (let i = 0 ; i< ref.current.children.length ; i ++){
+      let x = ref.current.children[i].offsetHeight
+      let y =ref.current.children[i].offsetWidth
+      console.log(x," ",y,' ',ref.current.children[i].offsetTop , " ", ref.current.children[i].offsetLeft )
+    }
+  },[])
+  const onDrag = (e,data  )=>{
+
+    console.log('Event: ',e.x, " ",e.y);
+    //console.log('Data: ', data);
+    for (let i = 0 ; i< ref.current.children.length ; i ++){
+      let x = ref.current.children[i].offsetTop
+      let y =ref.current.children[i].offsetLeft
+      console.log(Math.sqrt(((x-e.x)*(x-e.x)) + ((y-e.y)*(y-e.y))))
+    }
+
+    
+  
+  
+  }
+  
   return (
     <div style={{
       height:"100vh", 
@@ -22,19 +49,34 @@ function App() {
         alignItems:"center" , 
         flexDirection:"column"
      }}>
+
+
+
+
+      <Draggable
+               
+                onDrag={onDrag}
+        
+      >
       <div style={{
           display:"flex",
 
         }}>
-      {randomN.map(number=>{
+      {test.map(number=>{
         return <p style={{marginLeft:"2rem"}}>{number}</p>
       })}
       </div>
-      <div style={{
+      </Draggable>
+      <div ref ={ref}style={{
         display:"flex"
       }}>
        {randomN.map(number=>{
-        return <p style={{marginLeft:"2rem"}}>a</p>
+        return <input style={{
+          marginLeft:"2rem" , 
+          height:"3em" , 
+          width:"3em"
+        
+        }} />
       })}
       </div>
       </div>
