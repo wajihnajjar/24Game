bed21e39a7b5c7db7dd19ee86db0f173f23da52f
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-03 12:33:23 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const numberRef = useRef(null)
   const [randomN , setRand] = useState([])
   const [allLevels , setLeveles] = useState([])
-  const [currLevel , setCurrLevel] = useState(12)
+  const [currLevel , setCurrLevel] = useState(13)
   const [test,setTest] = useState(["","","","","","",""])
   const [sum , setSum] = useState(0)
   const [lowestInter , setLowerInter] = useState(8)
@@ -122,7 +122,7 @@ console.log('************')
 
     }
     setSum(stack[0])
-    if(currLevel!=14){
+    if(currLevel!=13){
     if(stack[0]==24){
       alert.success('Level Finished', {
         timeout: 4000, // custom timeout just for this one alert
