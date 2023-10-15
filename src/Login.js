c80d87d776a4ce3b607ad303d0f3e5117c558125
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-15 01:36:41 +0100

    Working on Login

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,15 @@ import React from "react";
 export default  function Login () {
 
 return (
-    <div>
-        
+    <div style={{
+        height:"100vh" , 
+        width:"10wh", 
+        backgroundColor:"red",
+        display:"grid", 
+        gridTemplateColumns: "25% 50% 25%" ,
+
+    }}>
+        test
     </div>
 )
 
